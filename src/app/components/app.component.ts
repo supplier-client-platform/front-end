124f617314cd1d34899a483b582a067ad5c26995
= <=>
2016-09-22 16:23:21 +0530

    integrated ng-lightning

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -3,9 +3,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['app.component.scss']
 })
 
 export class AppComponent {
   title: string = 'App works! Supplier Client Angular2 App';
+
+
+  selected: boolean = true;
+  iconType: string = 'border';
+  sizes: string[] = ['x-small', 'small', 'large'];
+
+  change() {
+    this.selected = !this.selected;
+    this.iconType = this.iconType === 'border' ? 'container' : 'border';
+  }
 }
--- src/app/components/app.module.ts
+++ src/app/components/app.module.ts
@@ -2,16 +2,19 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 
+import {NglModule, provideNglConfig} from 'ng-lightning/ng-lightning';
 import { AppComponent }  from './app.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    NglModule
   ],
   declarations: [
     AppComponent
   ],
+  providers: [provideNglConfig({'svgPath': './icons'})],
   bootstrap: [AppComponent]
 })
 
