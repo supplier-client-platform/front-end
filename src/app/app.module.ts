d843356a12884d84a0deb07720dc5b294b050ca5
= <=>
2016-09-26 13:00:10 +0530

    configured header and sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { FormsModule }   from '@angular/forms';
 
 import {NglModule, provideNglConfig} from 'ng-lightning/ng-lightning';
 import { AppComponent }  from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
 
 @NgModule({
   imports: [
@@ -12,7 +14,9 @@ import { AppComponent }  from './app.component';
     NglModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   providers: [provideNglConfig({'svgPath': './icons'})],
   bootstrap: [AppComponent]
--- src/app/index.ts
+++ src/app/index.ts
@@ -1,1 +1,1 @@
-export * from './components';
+export * from './app.module';
