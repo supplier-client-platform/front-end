b14d2da7d303d5f05cf81c475d1687dc65df8fe1
rishanthakumarrasarathinam <rishanthakumar@gmail.com>
2016-10-07 23:30:15 +0530

    Added order-list-table-component

---
+++ src/app/pages/orders/order-list/order-list-table/order-list-table.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { OrderListTableComponent } from './order-list-table.component';
+
+describe('Component: OrderListTable', () => {
+  it('should create an instance', () => {
+    let component = new OrderListTableComponent();
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/orders/order-list/order-list-table/order-list-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order-list-table',
+  templateUrl: './order-list-table.component.html',
+  styleUrls: ['./order-list-table.component.scss']
+})
+export class OrderListTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
