20f89ace9a9ee141eba8b4ee7f19cbca6605427c
rishanthakumarrasarathinam <rishanthakumar@gmail.com>
2016-10-17 20:22:27 +0530

    Advance search component and ui form configured.

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { OrderService } from '../../shared/services/order.service';
+import { CommonService } from '../../shared/services/common.service';
+import {IOrderStatusSubmit} from '../orders/order-list/order-list.component';
 
 @Component({
   selector: 'app-search',
@@ -6,12 +9,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
+  @ViewChild('lgModal') lgModal;
+  searchOrders: Array<any> = [];
+  orderInfo: any;
+  orderInfoStatus: string;
 
-  constructor() { }
+  constructor(private commonService: CommonService, private orderService: OrderService) { }
 
   ngOnInit() {
 
   }
 
+  getSearchOrders(value) {
+    let param = this.commonService.addQueryParams({
+      marketPlaceId: 1,
+      orderId: value.orderId,
+      customer_name: value.name,
+      contact_number: value.customer_mobile,
+      startDate: value.from,
+      endDate: value.to
+
+    }, ['']);
+    this.orderService.getOrders(param)
+      .subscribe((data: any) => {
+        this.searchOrders = data.data;
+      });
+  }
+
+  loadOrder(order) {
+    this.orderInfo = order;
+  }
+
+  loadModal(status) {
+    this.orderInfoStatus = status;
+    this.lgModal.show();
+  }
+
+  orderStatusSubmit(values: any) {
+    console.log(values.reason);
+
+    let obj: IOrderStatusSubmit = {
+      orderID: this.orderInfo.id,
+      status: this.orderInfoStatus,
+      reason: values.reason
+    };
+
+    // TODO need to handle this after a success response from the server otherwise need to give a proper error message.
+
+    this.lgModal.hide();
+
+    this.orderService.changeOrderStatus(obj).subscribe((data: any) => {
+      this.orderInfo.status = this.orderInfoStatus;
+    }, (err) => {
+      console.log(err);
+    });
+  }
 
 }
